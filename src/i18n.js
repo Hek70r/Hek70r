adb3ba9f6f8415dfff3c9ff0d2f244708f6a3ca3
Wiktor Mucha <wmucha12@vp.pl>
2024-04-05 02:38:25 +0200

    Removed syntax error

--- src/i18n.js
+++ src/i18n.js
@@ -3,11 +3,6 @@ import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-const resources = {
-  en: {
-    translation:
-  },
-};
 i18n
   .use(HttpBackend)
   .use(LanguageDetector)

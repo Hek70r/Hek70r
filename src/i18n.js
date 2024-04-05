19346281550051ea7d7ac9cba1cdc80ecccc572a
Wiktor Mucha <wmucha12@vp.pl>
2024-04-05 02:34:05 +0200

    Update i18n-config.js

--- src/i18n.js
+++ src/i18n.js
@@ -2,13 +2,24 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import translationEN from ".././public/locales/en/translation.json";
+import translationPL from ".././public/locales/pl/translation.json";
 
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  pl: {
+    translation: translationPL,
+  },
+};
 i18n
   .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: "en", // default language
+    resources,
+    lng: "en",
     debug: true,
     detection: {
       order: [
